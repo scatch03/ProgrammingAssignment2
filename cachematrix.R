683182228e67b678ac0299fd431cb45bd554b0df
a.vorona <a.vorona@itmgmedia.net>
2019-03-11 22:12:54 +0200

    Week 2 assignment, first attempt

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create matrix wrapper implementation for holding  
+## cached inverse matrix value which is obtained by relatively
+## heavy computing operation
 
-## Write a short comment describing this function
-
+## wrap matrix and lazily-computed inverse matrix value together
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
+## where 'x' is obtained by calling  'makeCacheMatrix'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
